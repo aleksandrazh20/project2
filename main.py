ca33968fafdb7f938bc3564737e9600f01616e97
aleksandrazh20 <hollandjoe20@gmail.com>
2019-10-07 20:12:48 +0700

    init

---
+++ main.py
@@ -0,0 +1,318 @@
+# Program gets woman's zodiac sign and man's zodiac sign.
+# (Aries-1, Taurus-2, Gemini-3, Cancer-4, Leo-5, Virgo-6,
+# Libra-7, Scorpio-8, Saggitarius-9, Capricorn-10, Aquarius-11, Pisces-12)
+# The program shows the horoscope compatibility between 2 people.
+
+womansign = int(input())
+mansign = int(input())
+if womansign == 1 :
+    if mansign == 1 :
+        print(60)
+    elif mansign == 2 :
+        print(65)
+    elif mansign == 3 :
+        print(65)
+    elif mansign == 4 :
+        print(65)
+    elif mansign == 5 :
+        print(90)
+    elif mansign == 6 :
+        print(45)
+    elif mansign == 7 :
+        print(70)
+    elif mansign == 8 :
+        print(80)
+    elif mansign == 9 :
+        print(90)
+    elif mansign == 10 :
+        print(50)
+    elif mansign == 11 :
+        print(55)
+    else :
+        print(65)
+
+elif womansign == 2 :
+    if mansign == 1:
+        print(60)
+    elif mansign == 2:
+        print(70)
+    elif mansign == 3:
+        print(70)
+    elif mansign == 4:
+        print(80)
+    elif mansign == 5:
+        print(70)
+    elif mansign == 6:
+        print(90)
+    elif mansign == 7:
+        print(75)
+    elif mansign == 8:
+        print(85)
+    elif mansign == 9:
+        print(50)
+    elif mansign == 10:
+        print(95)
+    elif mansign == 11:
+        print(80)
+    else:
+        print(85)
+
+elif womansign == 3 :
+    if mansign == 1 :
+        print(70)
+    elif mansign == 2 :
+        print(70)
+    elif mansign == 3 :
+        print(75)
+    elif mansign == 4 :
+        print(60)
+    elif mansign == 5 :
+        print(80)
+    elif mansign == 6 :
+        print(75)
+    elif mansign == 7 :
+        print(90)
+    elif mansign == 8 :
+        print(60)
+    elif mansign == 9 :
+        print(75)
+    elif mansign == 10 :
+        print(50)
+    elif mansign == 11 :
+        print(90)
+    else :
+        print(50)
+
+elif womansign == 4 :
+    if mansign == 1 :
+        print(65)
+    elif mansign == 2 :
+        print(80)
+    elif mansign == 3 :
+        print(60)
+    elif mansign == 4 :
+        print(75)
+    elif mansign == 5 :
+        print(70)
+    elif mansign == 6 :
+        print(75)
+    elif mansign == 7 :
+        print(60)
+    elif mansign == 8 :
+        print(95)
+    elif mansign == 9 :
+        print(55)
+    elif mansign == 10 :
+        print(45)
+    elif mansign == 11 :
+        print(70)
+    else :
+        print(90)
+
+elif womansign == 5 :
+    if mansign == 1 :
+        print(90)
+    elif mansign == 2 :
+        print(70)
+    elif mansign == 3 :
+        print(80)
+    elif mansign == 4 :
+        print(70)
+    elif mansign == 5 :
+        print(85)
+    elif mansign == 6 :
+        print(75)
+    elif mansign == 7 :
+        print(65)
+    elif mansign == 8 :
+        print(75)
+    elif mansign == 9 :
+        print(95)
+    elif mansign == 10 :
+        print(45)
+    elif mansign == 11 :
+        print(70)
+    else :
+        print(90)
+
+elif womansign == 6 :
+    if mansign == 1 :
+        print(45)
+    elif mansign == 2 :
+        print(90)
+    elif mansign == 3 :
+        print(75)
+    elif mansign == 4 :
+        print(75)
+    elif mansign == 5 :
+        print(75)
+    elif mansign == 6 :
+        print(70)
+    elif mansign == 7 :
+        print(80)
+    elif mansign == 8 :
+        print(85)
+    elif mansign == 9 :
+        print(70)
+    elif mansign == 10 :
+        print(95)
+    elif mansign == 11 :
+        print(50)
+    else :
+        print(70)
+
+elif womansign == 7 :
+    if mansign == 1 :
+        print(70)
+    elif mansign == 2 :
+        print(75)
+    elif mansign == 3 :
+        print(90)
+    elif mansign == 4 :
+        print(60)
+    elif mansign == 5 :
+        print(65)
+    elif mansign == 6 :
+        print(80)
+    elif mansign == 7 :
+        print(80)
+    elif mansign == 8 :
+        print(85)
+    elif mansign == 9 :
+        print(80)
+    elif mansign == 10 :
+        print(85)
+    elif mansign == 11 :
+        print(95)
+    else :
+        print(50)
+
+elif womansign == 8 :
+    if mansign == 1 :
+        print(80)
+    elif mansign == 2 :
+        print(85)
+    elif mansign == 3 :
+        print(60)
+    elif mansign == 4 :
+        print(95)
+    elif mansign == 5 :
+        print(75)
+    elif mansign == 6 :
+        print(85)
+    elif mansign == 7 :
+        print(85)
+    elif mansign == 8 :
+        print(90)
+    elif mansign == 9 :
+        print(80)
+    elif mansign == 10 :
+        print(65)
+    elif mansign == 11 :
+        print(60)
+    else :
+        print(95)
+
+elif womansign == 9 :
+    if mansign == 1 :
+        print(90)
+    elif mansign == 2 :
+        print(50)
+    elif mansign == 3 :
+        print(75)
+    elif mansign == 4 :
+        print(55)
+    elif mansign == 5 :
+        print(95)
+    elif mansign == 6 :
+        print(70)
+    elif mansign == 7 :
+        print(80)
+    elif mansign == 8 :
+        print(85)
+    elif mansign == 9 :
+        print(85)
+    elif mansign == 10 :
+        print(55)
+    elif mansign == 11 :
+        print(60)
+    else :
+        print(75)
+
+elif womansign == 10 :
+    if mansign == 1 :
+        print(50)
+    elif mansign == 2 :
+        print(95)
+    elif mansign == 3 :
+        print(50)
+    elif mansign == 4 :
+        print(45)
+    elif mansign == 5 :
+        print(45)
+    elif mansign == 6 :
+        print(95)
+    elif mansign == 7 :
+        print(85)
+    elif mansign == 8 :
+        print(65)
+    elif mansign == 9 :
+        print(55)
+    elif mansign == 10 :
+        print(85)
+    elif mansign == 11 :
+        print(70)
+    else :
+        print(85)
+
+elif womansign == 11 :
+    if mansign == 1 :
+        print(55)
+    elif mansign == 2 :
+        print(80)
+    elif mansign == 3 :
+        print(90)
+    elif mansign == 4 :
+        print(70)
+    elif mansign == 5 :
+        print(70)
+    elif mansign == 6 :
+        print(50)
+    elif mansign == 7 :
+        print(95)
+    elif mansign == 8 :
+        print(60)
+    elif mansign == 9 :
+        print(60)
+    elif mansign == 10 :
+        print(70)
+    elif mansign == 11 :
+        print(80)
+    else :
+        print(55)
+
+else :
+    if mansign == 1 :
+        print(65)
+    elif mansign == 2 :
+        print(85)
+    elif mansign == 3 :
+        print(50)
+    elif mansign == 4 :
+        print(90)
+    elif mansign == 5 :
+        print(90)
+    elif mansign == 6 :
+        print(70)
+    elif mansign == 7 :
+        print(50)
+    elif mansign == 8 :
+        print(95)
+    elif mansign == 9 :
+        print(75)
+    elif mansign == 10 :
+        print(85)
+    elif mansign == 11 :
+        print(55)
+    else :
+        print(80)
